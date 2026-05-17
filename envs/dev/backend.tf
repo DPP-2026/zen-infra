dbee5b0c288893e5c7b77d99a4bce39fd73876dc
vsawantvinay <vsawant.azure@gmail.com>
2026-05-18 00:34:29 +0530

    added S3 bucket right name

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -2,7 +2,7 @@
 
 terraform {
   backend "s3" {
-    bucket       = "zen-pharma-terraform-vsawantvinay"  # Replace with your S3 bucket name
+    bucket       = "zen-pharma-terraform-state-vsawantvinay"  # Replace with your S3 bucket name
     key          = "envs/dev/terraform.tfstate"
     region       = "ap-south-1"
     encrypt      = true
