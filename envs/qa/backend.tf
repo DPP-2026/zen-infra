f36688d4c9ce88cbbc90fcd072dd8ef34966262f
Ranjini Ganeshan <32661402+ranjiniganeshan@users.noreply.github.com>
2026-08-02 07:43:33 +0530

    Update S3 bucket name for QA backend

--- envs/qa/backend.tf
+++ envs/qa/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "zen-pharma-terraform-state-ranjiniganeshan"
+    bucket       = "zen-pharma-terraform-state-dpp-2026"
     key          = "envs/qa/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
